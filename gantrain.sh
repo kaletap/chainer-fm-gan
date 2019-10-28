99d16a4cff1db5fa96521c318996bff84f904832
Przemyslaw Kaleta <przemek2889@gmail.com>
2019-10-28 14:07:59 +0100

    Added requirements file.

--- gantrain.sh
+++ gantrain.sh
@@ -2,6 +2,6 @@
 
 GPU=${GPU:--1}
 
-python -u gan_train.py \
+python3 -u gan_train.py \
  -b 128 --log-interval 100 -g ${GPU} --test data/prep-title.txt \
   data/prep-title.txt data/vocab.txt --valid-interval 50 --epoch 400
